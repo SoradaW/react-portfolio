bdee11dc39c7f3db7a23584ea10917a25213f048
Mrs SORADA WRIGHT <93084381+SoradaW@users.noreply.github.com>
2023-03-04 20:27:05 +0000

    Added .json file for projects  and work files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import Home from "./Sections/Home";
+import About from "./Sections/About";
+import Work from "./Sections/Work";
+import Contact from "./Sections/Contact";
 import Navbar from "./Components/Navbar";
 import ContactBar from "./Components/ContactBar";
 import Footer from "./Components/Footer";
@@ -10,6 +13,8 @@ function App() {
 			<Navbar />
       <div className="app_section_container">
         <Home />
+        <About />
+        <Work />
       </div>
 			<ContactBar />
 			<Footer />
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -10,14 +10,11 @@ function Navbar() {
 				<a href="#about" className={styles.nav_link}>
 					<span>01.</span>About
 				</a>
-				<a href="#experience" className={styles.nav_link}>
-					<span>02.</span>Experience
-				</a>
 				<a href="#work" className={styles.nav_link}>
-					<span>03.</span>Work
+					<span>02.</span>Projects
 				</a>
 				<a href="#contact" className={styles.nav_link}>
-					<span>04.</span>Contact
+					<span>03.</span>Contact
 				</a>
 				<button className={styles.nav_resume_btn}>Resume</button>
 			</div>
