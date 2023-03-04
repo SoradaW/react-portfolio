2b10046b4cdd4e58e8d80d7d4e066fe23909cdc2
Mrs SORADA WRIGHT <93084381+SoradaW@users.noreply.github.com>
2023-03-04 13:52:07 +0000

    Added index.jsx code

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,28 @@
+import styles from "./styles.module.css";
+
+function Navbar () {
+  return (
+    <nav className={styles.nav-container}>
+      <div className={styles.nav-left}>
+        <a href="#home">SW</a>
+      </div>
+      <div className={styles.nav-right}>
+        <a href="#about" className={styles.nav-link}>
+          <span>01.</span>About
+        </a>
+        <a href="#experience" className={styles.nav-link}>
+          <span>02.</span>Experience
+        </a>
+        <a href="#work" className={styles.nav-link}>
+          <span>03.</span>Work
+        </a>
+        <a href="#contact" className={styles.nav-link}>
+          <span>04.</span>Contact
+        </a>
+        <button className={styles.nav-resume-btn}>Resume</button>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
