3843166494c4931c0f206904820537267d2c47f5
Mrs SORADA WRIGHT <93084381+SoradaW@users.noreply.github.com>
2023-03-04 15:05:33 +0000

    Added contact links/icons

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,35 @@
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import styles from "./styles.module.css";
+
+function ContactBar() {
+	return (
+		<div className={styles.contact-bar-wrapper}>
+			<div className={styles.contact-bar}>
+				<a href="/" target="-blank" className={styles.icon} >
+					<LinkedInIcon  />
+				</a>
+				<a href="/" target="-blank" className={styles.icon} >
+					<GitHubIcon  />
+				</a>
+				<a href="/" target="-blank" className={styles.icon} >
+					<FacebookIcon  />
+				</a>
+				<a href="/" target="-blank" className={styles.icon} >
+					<WhatsAppIcon  />
+				</a>
+				<div className={styles.line}></div>
+			</div>
+			<div className={styles.contact-bar}>
+				<a href="mailto:sorada.wright@gmail.com" className={styles.email}>
+					sorada.wright@gmail.com
+				</a>
+				<div className={styles.line}></div>
+			</div>
+		</div>
+	);
+}
+
+export default ContactBar;
